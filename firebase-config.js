4ed73f4d94beb2cf82b310b699ecedf1e8d3053a
kim <kayanee0102@gmail.com>
2026-05-26 07:41:22 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+// ⚠️ 이 파일은 .gitignore에 추가하세요
+// GitHub에 올리지 마세요
+
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyB4ka21mFE7avHmDLiOrOqM5LPLe4anUJo",
+  authDomain: "khu-map.firebaseapp.com",
+  projectId: "khu-map",
+  storageBucket: "khu-map.firebasestorage.app",
+  messagingSenderId: "750025397742",
+  appId: "1:750025397742:web:7380637d7da93ee8a7aaff"
+};
